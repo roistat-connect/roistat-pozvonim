c6ed5553210e41d68ce1cfdde4751dc116859f94
ildar r. khasanshin <0@10021987.ru>
2018-12-27 18:54:27 +0500

    + кодировка по умолчанию utf8

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ var PozvonimcomWidgetConfig = {
     w.roistatProjectId = id; w.roistatHost = h;
     var p = d.location.protocol == "https:" ? "https://" : "http://";
     var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init";
-    var js = d.createElement(s); js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
+    var js = d.createElement(s); js.charset = "UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
 })(window, document, 'script', 'cloud.roistat.com', '');
 </script>
 <!-- Код ройстат -->
